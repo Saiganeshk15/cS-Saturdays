a52acaeb78c9539ea0753c9591de1b7d542a4994
pranav6004 <pranav99630@gmail.com>
2024-11-14 18:43:50 +0530

    updated years and branches

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -196,7 +196,7 @@ export default function Form() {
               label="Year"
               required
               data={[
-                // { value: "1", label: "1" },
+                { value: "1", label: "1" },
                 { value: "2", label: "2" },
                 { value: "3", label: "3" },
                 { value: "4", label: "4" },
@@ -213,6 +213,7 @@ export default function Form() {
                 { value: "B", label: "B" },
                 { value: "C", label: "C" },
                 { value: "D", label: "D" },
+                { value: "Other",label: "Other"}
               ]}
               placeholder="Enter your section"
               {...form.getInputProps("section")}
